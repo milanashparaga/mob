21394af7544d32ff50d16c3a4828bc03824bdabc
Милана Шпарага <milanashparaga@yandex.ru>
2024-10-03 22:02:49 +0300

    Проект(ВТОРОЙ СЕМЕСТР)

---
+++ homelibrary/app/src/main/java/ru/mirea/shparaga/homelibrary/data/repository/WeatherRepositoryImpl.java
@@ -0,0 +1,11 @@
+package ru.mirea.shparaga.homelibrary.data.repository;
+
+import ru.mirea.shparaga.homelibrary.domain.model.Weather;
+import ru.mirea.shparaga.homelibrary.domain.repository.WeatherRepository;
+
+public class WeatherRepositoryImpl implements WeatherRepository {
+    @Override
+    public Weather getWeatherData() {
+        return new Weather(22.5, 60, "Sunny");
+    }
+}
